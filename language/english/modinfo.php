7755434460e2cbf88e31065fe1ed356842c2a126
GregMage <gregory.mage@gmail.com>
2017-07-05 16:38:29 +0200

    Structure du module

---
+++ language/english/modinfo.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit authors.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+/**
+ * xmdoc module
+ *
+ * @copyright       XOOPS Project (http://xoops.org)
+ * @license         GNU GPL 2 (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @author          Mage Gregory (AKA Mage)
+ */
+// The name of this module
+define('_MI_XMDOC_NAME', 'Document');
+define('_MI_XMDOC_DESC', 'Document management');
+
+// Menu
+define('_MI_XMDOC_MENU_HOME', 'Index');
+define('_MI_XMDOC_MENU_CATEGORY', 'Category');
+define('_MI_XMDOC_MENU_DOCUMENT', 'Document');
+define('_MI_XMDOC_MENU_PERMISSION', 'Permission');
+define('_MI_XMDOC_MENU_ABOUT', 'About');
+
+// Sub menu
+
+
+// Pref
---
+++ xoops_version.php
@@ -0,0 +1,76 @@
+<?php
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit authors.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+/**
+ * xmdoc module
+ *
+ * @copyright       XOOPS Project (http://xoops.org)
+ * @license         GNU GPL 2 (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @author          Mage Gregory (AKA Mage)
+ */
+$modversion['dirname']     = basename(__DIR__);
+$modversion['name']        = ucfirst(basename(__DIR__));
+$modversion['version']     = '0.1';
+$modversion['description'] = _MI_XMDOC_DESC;
+$modversion['author']      = 'Grégory Mage (Mage)';
+$modversion['url']         = 'https://github.com/GregMage';
+$modversion['credits']     = 'Mage';
+
+$modversion['help']        = 'page=help';
+$modversion['license']     = 'GNU GPL 2 or later';
+$modversion['license_url'] = 'http://www.gnu.org/licenses/gpl-2.0.html';
+$modversion['official']    = 0;
+$modversion['image']       = 'assets/images/xmdoc_logo.png';
+
+// Menu
+/*$modversion['hasMain'] = 1;
+$modversion['sub'][] = array(
+    'name' => _MI_XMDOC_SUB_ADD,
+    'url'  => 'action.php?op=add'
+);*/
+
+// Admin things
+$modversion['hasAdmin']    = 1;
+$modversion['system_menu'] = 1;
+$modversion['adminindex']  = 'admin/index.php';
+$modversion['adminmenu']   = 'admin/menu.php';
+
+// Install and update
+//$modversion['onInstall']        = 'include/install.php';
+//$modversion['onUpdate']         = 'include/update.php';
+
+// Tables
+$modversion['sqlfile']['mysql'] = 'sql/mysql.sql';
+
+$modversion['tables'][1] = 'xmdoc_category';
+$modversion['tables'][2] = 'xmdoc_doc';
+
+// Admin Templates
+/*$modversion['templates'][] = array('file' => 'xmdoc_admin_category.tpl', 'description' => '', 'type' => 'admin');
+$modversion['templates'][] = array('file' => 'xmdoc_admin_doc.tpl', 'description' => '', 'type' => 'admin');
+$modversion['templates'][] = array('file' => 'xmdoc_admin_permission.tpl', 'description' => '', 'type' => 'admin');*/
+
+// User Templates
+//$modversion['templates'][] = array('file' => 'xmdoc_action.tpl', 'description' => '');
+
+// Configs
+
+
+// About stuff
+$modversion['module_status'] = 'Alpha';
+$modversion['release_date']  = '2017/07/05';
+
+$modversion['developer_lead']      = 'Mage';
+$modversion['module_website_url']  = 'github.com/GregMage';
+$modversion['module_website_name'] = 'github.com/GregMage';
+
+$modversion['min_xoops'] = '2.5.9';
+$modversion['min_php']   = '5.3.7';
