dc709a1df0f75e4e41d3e52821b9638c7c1b26d6
GregMage <gregory.mage@gmail.com>
2026-09-09 17:31:21 +0200

    Version 2.1.0-RC1

--- xoops_version.php
+++ xoops_version.php
@@ -18,7 +18,7 @@
  */
 $modversion['dirname']     = basename(__DIR__);
 $modversion['name']        = ucfirst(basename(__DIR__));
-$modversion['version']     = '2.0.0-Stable';
+$modversion['version']     = '2.1.0-RC1';
 $modversion['description'] = _MI_XMDOC_DESC;
 $modversion['author']      = 'Grégory Mage (Mage)';
 $modversion['url']         = 'https://github.com/GregMage';
@@ -310,7 +310,7 @@ $modversion['config'][] = array(
 );
 
 // About stuff
-$modversion['release_date']  = '2026/05/26';
+$modversion['release_date']  = '2026/09/09';
 
 $modversion['developer_lead']      = 'Mage';
 $modversion['module_website_url']  = 'www.monxoops.fr/';
